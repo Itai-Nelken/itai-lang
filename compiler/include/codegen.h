4bb8a70bacd3cd3889bb0efc9916ebefb4506922
Itai-Nelken <itainelken@gmail.com>
2022-03-30 18:51:05 +0300

    Codegen: use register allocation
    instead of generating code for a stack machine:
    old (pseudo code):
    ---
    mov R0, 1
    push R0
    mov R0, 2
    pop R1
    add R0, R0, R1
    ---
    
    new (pseudo code):
    ---
    mov R0, 1
    mov R1, 1
    add R0, R0, R1
    ---
    
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/include/codegen.h
+++ compiler/include/codegen.h
@@ -5,9 +5,16 @@
 #include <stdbool.h>
 #include "ast.h"
 
+typedef enum registers {
+    R0, R1, R2, R3,
+    _REG_COUNT
+} Register;
+
 typedef struct code_generator {
     ASTProg *program;
     FILE *out;
+
+    bool free_regs[_REG_COUNT];
 } CodeGenerator;
 
 void initCodegen(CodeGenerator *cg, ASTProg *program, FILE *file);
--- compiler/src/codegen.c
+++ compiler/src/codegen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> // memset()
 #include <stdarg.h>
 #include "common.h"
 #include "codegen.h"
@@ -6,9 +7,38 @@
 void initCodegen(CodeGenerator *cg, ASTProg *program, FILE *file) {
     cg->out = file;
     cg->program = program;
+    memset(cg->free_regs, true, _REG_COUNT);
 }
 void freeCodegen(CodeGenerator *cg) {
     // nothing
+}
+
+// TODO: register spilling
+static Register allocate_register(CodeGenerator *cg) {
+    for(int i = 0; i < _REG_COUNT; ++i) {
+        if(cg->free_regs[i]) {
+            cg->free_regs[i] = false;
+            return (Register)i;
+        }
+    }
+    // no registers left
+    UNREACHABLE();
+}
+
+static void free_register(CodeGenerator *cg, Register reg) {
+    if(cg->free_regs[reg]) {
+        // double free
+        UNREACHABLE();
+    }
+    cg->free_regs[reg] = true;
+}
+
+static const char *registers[_REG_COUNT] = {"x0", "x1", "x2", "x3"};
+static const char *reg_to_str(Register reg) {
+    if(reg > _REG_COUNT) {
+        UNREACHABLE();
+    }
+    return registers[reg];
 }
 
 static void println(CodeGenerator *cg, const char *format, ...) {
@@ -20,94 +50,101 @@ static void println(CodeGenerator *cg, const char *format, ...) {
     fputs("\n", cg->out);
 }
 
-static void gen_expr(CodeGenerator *cg, ASTNode *expr) {
+// returns the register that will contain the result
+static Register gen_expr(CodeGenerator *cg, ASTNode *expr) {
     if(expr->type == ND_NUM) {
-        println(cg, "mov x0, %d", expr->literal.int32);
-        return;
+        Register reg = allocate_register(cg);
+        println(cg, "mov %s, %d", reg_to_str(reg), expr->literal.int32);
+        return reg;
     } else if(expr->type == ND_NEG) {
-        gen_expr(cg, expr->left);
-        println(cg, "neg x0, x0");
-        return;
+        Register reg = gen_expr(cg, expr->left);
+        println(cg, "neg %s, %s", reg_to_str(reg), reg_to_str(reg));
+        return reg;
     }
 
-    // first generate the right side because
-    // we want right to be in x1 and left to be in x0.
-    // this can be achieved in different ways.
-    gen_expr(cg, expr->right);
-    println(cg, "str x0, [sp, -16]!"); // push x0
-    gen_expr(cg, expr->left);
-    println(cg, "ldr x1, [sp], 16"); // pop x1
+    Register leftreg = gen_expr(cg, expr->left);
+    Register rightreg = gen_expr(cg, expr->right);
+
+    const char *left = reg_to_str(leftreg);
+    const char *right = reg_to_str(rightreg);
 
     switch(expr->type) {
         case ND_ADD:
-            println(cg, "add x0, x0, x1");
+            println(cg, "add %s, %s, %s", left, left, right);
             break;
         case ND_SUB:
-            println(cg, "sub x0, x0, x1");
+            println(cg, "sub %s, %s, %s", left, left, right);
             break;
         case ND_MUL:
-            println(cg, "mul x0, x0, x1");
+            println(cg, "mul %s, %s, %s", left, left, right);
             break;
         case ND_DIV:
             // use 'udiv' for unsigned values
-            println(cg, "sdiv x0, x0, x1");
+            println(cg, "sdiv %s, %s, %s", left, left, right);
             break;
-        case ND_REM:
+        case ND_REM: {
             // tmp = a / b
             // tmp = tmp * b
             // result = tmp - b
-            println(cg, "sdiv x2, x0, x1");
-            println(cg, "mul x1, x2, x1");
-            println(cg, "sub x0, x0, x1");
+            Register tmp = allocate_register(cg);
+            println(cg, "sdiv %s, %s, %s", reg_to_str(tmp), left, right);
+            println(cg, "mul %s, %s, %s", right, reg_to_str(tmp), right);
+            println(cg, "sub %s, %s, %s", left, left, right);
+            free_register(cg, tmp);
             break;
+        }
         case ND_EQ:
-            println(cg, "cmp x0, x1");
-            println(cg, "cset x0, eq");
+            println(cg, "cmp %s, %s", left, right);
+            println(cg, "cset %s, eq", left);
             break;
         case ND_NE:
-            println(cg, "cmp x0, x1");
-            println(cg, "cset x0, ne");
+            println(cg, "cmp %s, %s", left, right);
+            println(cg, "cset %s, ne", left);
             break;
         case ND_GT:
-            println(cg, "cmp x0, x1");
-            println(cg, "cset x0, gt");
+            println(cg, "cmp %s, %s", left, right);
+            println(cg, "cset %s, gt", left);
             break;
         case ND_GE:
-            println(cg, "cmp x0, x1");
-            println(cg, "cset x0, ge");
+            println(cg, "cmp %s, %s", left, right);
+            println(cg, "cset %s, ge", left);
             break;
         case ND_LT:
-            println(cg, "cmp x0, x1");
-            println(cg, "cset x0, lt");
+            println(cg, "cmp %s, %s", left, right);
+            println(cg, "cset %s, lt", left);
             break;
         case ND_LE:
-            println(cg, "cmp x0, x1");
-            println(cg, "cset x0, le");
+            println(cg, "cmp %s, %s", left, right);
+            println(cg, "cset %s, le", left);
             break;
         case ND_BIT_OR:
-            println(cg, "orr x0, x0, x1");
+            println(cg, "orr %s, %s, %s", left, left, right);
             break;
         case ND_XOR:
-            println(cg, "eor x0, x0, x1");
+            println(cg, "eor %s, %s, %s", left, left, right);
             break;
         case ND_BIT_AND:
-            println(cg, "and x0, x0, x1");
+            println(cg, "and %s, %s, %s", left, left, right);
             break;
         case ND_BIT_RSHIFT:
-            println(cg, "lsr x0, x0, x1");
+            println(cg, "lsr %s, %s, %s", left, left, right);
             break;
         case ND_BIT_LSHIFT:
-            println(cg, "lsl x0, x0, x1");
+            println(cg, "lsl %s, %s, %s", left, left, right);
             break;
         default:
             UNREACHABLE();
     }
+
+    free_register(cg, rightreg);
+    return leftreg;
 }
 
 void gen_stmt(CodeGenerator *cg, ASTNode *node) {
     if(node->type != ND_EXPR_STMT) {
         UNREACHABLE();
     }
+    // FIXME: what if result isn't always in x0 (R0)?
     gen_expr(cg, node->left);
 }
 
