5ded6dab6ec899514d11578e99a2fc674c216c91
Itai-Nelken <itainelken@gmail.com>
2022-06-27 21:19:40 +0300

    Compiler: improve pretty printing.
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
 	for(size_t i = 0; i < prog.globals.used; ++i) {
 		ASTNode *g = ARRAY_GET_AS(ASTNode *, &prog.globals, i);
 		ASTIdentifierNode *id_node = g->type == ND_ASSIGN ? AS_IDENTIFIER_NODE(AS_BINARY_NODE(g)->left) : AS_IDENTIFIER_NODE(g);
-		printf("%d) name: '%s', type: {", id_node->id, GET_SYMBOL_AS(ASTIdentifier, &prog.identifiers, id_node->id)->text);
+		printf("(id: %d) name: '%s', type: {", id_node->id, GET_SYMBOL_AS(ASTIdentifier, &prog.identifiers, id_node->id)->text);
 		printType(id_node->type);
 		puts("}");
 	}
@@ -39,7 +39,8 @@ int main(int argc, char **argv) {
 	for(size_t i = 0; i < prog.functions.used; ++i) {
 		ASTFunction *fn = ARRAY_GET_AS(ASTFunction *, &prog.functions, i);
 		ASTIdentifier *name = GET_SYMBOL_AS(ASTIdentifier, &prog.identifiers, fn->name->id);
-		printf("\x1b[32m%s\x1b[0m:\n", name->text);
+		printf("\x1b[1;32m%s\x1b[0m:\n", name->text);
+		printf("name id: %d\n", fn->name->id);
 		printf("return type: {");
 		printType(fn->return_type);
 		puts("}");
@@ -48,7 +49,7 @@ int main(int argc, char **argv) {
 		for(size_t i = 0; i < fn->locals.used; ++i) {
 			ASTNode *l = ARRAY_GET_AS(ASTNode *, &fn->locals, i);
 			ASTIdentifierNode *id_node = l->type == ND_ASSIGN ? AS_IDENTIFIER_NODE(AS_BINARY_NODE(l)->left) : AS_IDENTIFIER_NODE(l);
-			printf("%d) name: '%s', type: {", id_node->id, GET_SYMBOL_AS(ASTIdentifier, &prog.identifiers, id_node->id)->text);
+			printf("(id: %d) name: '%s', type: {", id_node->id, GET_SYMBOL_AS(ASTIdentifier, &prog.identifiers, id_node->id)->text);
 			printType(id_node->type);
 			puts("}");
 		}
