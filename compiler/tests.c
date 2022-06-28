37cd38ab4937c2f4952f71abe430f9041f2e4ff1
Itai-Nelken <itainelken@gmail.com>
2022-06-28 15:04:34 +0300

    Compiler: fix and improve tests.
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/tests.c
+++ compiler/tests.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 #include "common.h"
+#include "Strings.h"
 #include "Array.h"
 #include "Table.h"
 #include "Scanner.h"
@@ -12,19 +13,51 @@ typedef struct test {
     bool (*fn)();
 } Test;
 
+static const char *failed_expr = NULL;
+#define CHECK(expr) do { if(!(expr)) { failed_expr = #expr; return false; } } while(0)
+
 void print(Test *t, bool success) {
-    const char *result = success ? "\x1b[1;32mPassed\x1b[0m" : "\x1b[1;31mFailed\x1b[0m";
-    printf("%s: %s\n", t->name, result);
+    printf("%s: ", t->name);
+    if(!success) {
+        printf("'%s': ", failed_expr);
+        failed_expr = NULL;
+    }
+    printf("%s\x1b[0m\n", success ? "\x1b[1;32mPassed" : "\x1b[1;31mFailed");
+}
+
+static bool test_strings() {
+    char *s1 = newString(5);
+    stringAppend(s1, "Hello, %s!", "World");
+    CHECK(stringIsValid(s1));
+    CHECK(!strcmp(s1, "Hello, World!"));
+    CHECK(stringEqual(s1, "Hello, World!"));
+
+    // stringDUplicate() & stringCopy() use stringNCopy(), so it's also tested.
+    char *s2 = stringDuplicate(s1);
+    char *s3 = stringCopy("Hello, World!");
+    CHECK(stringEqual(s2, s3));
+
+    freeString(s2);
+    freeString(s3);
+    freeString(s1);
+    return true;
 }
 
 static void test_array_callback(void *item, void *cl) {
     static long i = 1;
     bool *ok = (bool *)cl;
-    *ok = ((long)item) == i;
+    if(!*ok) {
+        return;
+    }
+    if(!(((long)item) == i)) {
+        *ok = false;
+        failed_expr = "(long)item == i";
+    }
     ++i;
 }
 static bool test_array() {
     long expected[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const size_t expected_size = sizeof(expected) / sizeof(expected[0]);
     bool ok = true;
     Array a;
     initArray(&a);
@@ -34,16 +67,18 @@ static bool test_array() {
     }
 
     for(int i = 0; i < ARRAY_INITIAL_CAPACITY + 2; ++i) {
-        ok = ((long)arrayGet(&a, i)) == expected[i];
+        CHECK(((long)arrayGet(&a, i)) == expected[i]);
     }
 
     Array copy;
     initArray(&copy);
     arrayCopy(&copy, &a);
     for(int i = 0; i < ARRAY_INITIAL_CAPACITY + 2; ++i) {
-        ok = ((long)arrayPop(&copy)) == expected[sizeof(expected)/sizeof(expected[0]) - i];
+        CHECK(((long)arrayPop(&copy)) == expected[expected_size-1 - i]);
     }
 
+    // can't use CHECK() here.
+    // instead the callback won't run if ok == false.
     arrayMap(&a, test_array_callback, (void *)&ok);
 
     freeArray(&copy);
@@ -51,17 +86,36 @@ static bool test_array() {
     return ok;
 }
 
+struct table_test_expected {
+    const char *s;
+    long i;
+};
+struct table_test {
+    struct table_test_expected *expected;
+    size_t expected_length;
+    bool *ok;
+};
+static long find_value_for_key(struct table_test *t, char *key) {
+    for(int i = 0; i < (int)t->expected_length; ++i) {
+        if(t->expected[i].s[0] == key[0]) {
+            return t->expected[i].i;
+        }
+    }
+    return -1;
+}
 static void test_table_callback(TableItem *item, void *cl) {
-    static long i = 1;
-    static char s = 'a';
-    bool *ok = (bool *)cl;
-    *ok = ((long)item->value) == i;
-    *ok = ((char *)item->key)[0] == s;
-    i++;
-    s++;
+    struct table_test *t = (struct table_test *)cl;
+    if(!t->ok) {
+        return;
+    }
+    if(!(((long)item->value) == find_value_for_key(t, (char *)item->key))) {
+        t->ok = false;
+        failed_expr = "(long)item->value == find_value_for_key(t, (char *)item->key)";
+        return;
+    }
 }
 static bool test_table() {
-    struct {const char *s; long i;} expected[] = {
+    struct table_test_expected expected[] = {
         {"a", 1},
         {"b", 2},
         {"c", 3},
@@ -88,11 +142,19 @@ static bool test_table() {
         tableSet(&t, (void *)expected[i].s, (void *)expected[i].i);
     }
 
-    tableMap(&t, test_table_callback, (void *)&ok);
 
-    ok = ((long)tableGet(&t, (void *)"k")) == 11l;
+    CHECK(((long)(tableGet(&t, (void *)"k")->value)) == 11l);
     tableDelete(&t, (void *)"k");
-    ok = tableGet(&t, (void *)"k") == NULL;
+    CHECK(tableGet(&t, (void *)"k") == NULL);
+
+    struct table_test tt = {
+        .expected = expected,
+        .expected_length = sizeof(expected)/sizeof(expected[0]),
+        .ok = &ok
+    };
+    // can't use CHECK() here.
+    // instead the callback won't run if ok == false.
+    tableMap(&t, test_table_callback, (void *)&tt);
 
     freeTable(&t);
     return ok;
@@ -107,24 +169,17 @@ static bool test_scanner_1char() {
     };
     Scanner s;
     initScanner(&s, "Test (1 char)", "()[]{},;:~");
-    bool ok = true;
 
     Token tk = nextToken(&s);
     for(int idx = 0; idx < (int)(sizeof(expected)/sizeof(expected[0])); ++idx) {
-        if(tk.type == TK_EOF || tk.type == TK_ERROR) {
-            ok = false;
-            break;
-        }
-        if(!(ok = tk.type == expected[idx])) {
-            break;
-        }
+        CHECK(tk.type != TK_EOF || tk.type != TK_ERROR);
+        CHECK(tk.type == expected[idx]);
         tk = nextToken(&s);
     }
 
     freeScanner(&s);
-    return ok;
+    return true;
 }
-
 static bool test_scanner_1_2char() {
     TokenType expected[] = {
         TK_MINUS, TK_MINUS_EQUAL, TK_ARROW,
@@ -144,24 +199,17 @@ static bool test_scanner_1_2char() {
     // will be: TK_EQUAL_EQUAL, TK_EQUAL
     // instead of the other way round.
     initScanner(&s, "Test (1 or 2 char)", "- -= -> + += /= / * *= ! != == => = % %= ^ ^= | |= & &=");
-    bool ok = true;
 
     Token tk = nextToken(&s);
     for(int idx = 0; idx < (int)(sizeof(expected)/sizeof(expected[0])); ++idx) {
-        if(tk.type == TK_EOF || tk.type == TK_ERROR) {
-            ok = false;
-            break;
-        }
-        if(!(ok = tk.type == expected[idx])) {
-            break;
-        }
+        CHECK(tk.type != TK_EOF || tk.type != TK_ERROR);
+        CHECK(tk.type == expected[idx]);
         tk = nextToken(&s);
     }
 
     freeScanner(&s);
-    return ok;
+    return true;
 }
-
 static bool test_scanner_1_2_3char() {
     TokenType expected[] = {
         TK_GREATER, TK_GREATER_EQUAL, TK_RSHIFT, TK_RSHIFT_EQUAL,
@@ -170,24 +218,17 @@ static bool test_scanner_1_2_3char() {
     };
     Scanner s;
     initScanner(&s, "Test (1, 2 or 3 char)", "> >= >> >>= < <= << <<= ... .");
-    bool ok = true;
 
     Token tk = nextToken(&s);
     for(int idx = 0; idx < (int)(sizeof(expected)/sizeof(expected[0])); ++idx) {
-        if(tk.type == TK_EOF || tk.type == TK_ERROR) {
-            ok = false;
-            break;
-        }
-        if(!(ok = tk.type == expected[idx])) {
-            break;
-        }
+        CHECK(tk.type != TK_EOF || tk.type != TK_ERROR);
+        CHECK(tk.type == expected[idx]);
         tk = nextToken(&s);
     }
 
     freeScanner(&s);
-    return ok;
+    return true;
 }
-
 static bool test_scanner_literals() {
     TokenType expected[] = {
         TK_STRLIT,
@@ -199,37 +240,28 @@ static bool test_scanner_literals() {
     Scanner s;
     // TODO: hex, binary, and octal number literals
     initScanner(&s, "Test (literals)", "\"string\" 'c' 123 1.23 identifier");
-    bool ok = true;
 
     Token tk = nextToken(&s);
     for(int idx = 0; idx < (int)(sizeof(expected)/sizeof(expected[0])); ++idx) {
-        if(tk.type == TK_EOF || tk.type == TK_ERROR) {
-            ok = false;
-            break;
-        }
-        if(!(ok = tk.type == expected[idx])) {
-            break;
-        }
+        CHECK(tk.type != TK_EOF || tk.type != TK_ERROR);
+        CHECK(tk.type == expected[idx]);
         switch(tk.type) {
             case TK_STRLIT:
-                ok = strncmp(tk.lexeme, "\"string\"", tk.length) == 0;
+                CHECK(strncmp(tk.lexeme, "\"string\"", tk.length) == 0);
                 break;
             case TK_CHARLIT:
                 // 3 == ' <char> '
-                if(tk.length != 3) {
-                    ok = false;
-                    break;
-                }
-                ok = tk.lexeme[1] == 'c';
+                CHECK(tk.length == 3);
+                CHECK(tk.lexeme[1] == 'c');
                 break;
             case TK_NUMLIT:
-                ok = strtol(tk.lexeme, NULL, 10) == 123l;
+                CHECK(strtol(tk.lexeme, NULL, 10) == 123l);
                 break;
             case TK_FLOATLIT:
-                ok = strtod(tk.lexeme, NULL) == 1.23;
+                CHECK(strtod(tk.lexeme, NULL) == 1.23);
                 break;
             case TK_IDENTIFIER:
-                ok = strncmp(tk.lexeme, "identifier", tk.length) == 0;
+                CHECK(strncmp(tk.lexeme, "identifier", tk.length) == 0);
                 break;
             default:
                 UNREACHABLE();
@@ -238,9 +270,8 @@ static bool test_scanner_literals() {
     }
 
     freeScanner(&s);
-    return ok;
+    return true;
 }
-
 static bool test_scanner_keywords() {
     TokenType expected[] = {
         // types
@@ -286,26 +317,21 @@ static bool test_scanner_keywords() {
     };
     Scanner s;
     initScanner(&s, "Test (literals)", "i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 isize usize char str bool print var const public fn return enum struct if else switch module import as using while for type null typeof");
-    bool ok = true;
 
     Token tk = nextToken(&s);
     for(int idx = 0; idx < (int)(sizeof(expected)/sizeof(expected[0])); ++idx) {
-        if(tk.type == TK_EOF || tk.type == TK_ERROR) {
-            ok = false;
-            break;
-        }
-        if(!(ok = tk.type == expected[idx])) {
-            break;
-        }
+        CHECK(tk.type != TK_EOF || tk.type != TK_ERROR);
+        CHECK(tk.type == expected[idx]);
         tk = nextToken(&s);
     }
 
     freeScanner(&s);
-    return ok;
+    return true;
 }
 
-// TODO: Strings, Symbols, Parser
+// TODO: Symbols, Parser
 Test tests[] = {
+    {"Strings", test_strings},
     {"Array", test_array},
     {"Table", test_table},
     {"Scanner (1 character)", test_scanner_1char},
