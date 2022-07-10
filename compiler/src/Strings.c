40d3b40e25f9a67eb17e99f0d19e70f60738d464
Itai-Nelken <itainelken@gmail.com>
2022-07-10 18:02:07 +0300

    Compiler(Strings): Make code clearer.
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/src/Strings.c
+++ compiler/src/Strings.c
@@ -17,53 +17,60 @@
 // |___|___|___|___|___|___| ...
 //
 
+enum slots {
+    CAPACITY = 0,
+    LENGTH   = 1,
+    MAGIC    = 2,
+    SLOT_COUNT     = 3
+};
+
 static inline size_t *from_str(char *s) {
-    return ((size_t *)s) - 3;
+    return ((size_t *)s) - SLOT_COUNT;
 }
 
 static inline char *to_str(size_t *ptr) {
-    return (char *)(ptr + 3);
+    return (char *)(ptr + SLOT_COUNT);
 }
 
 char *newString(size_t capacity) {
     assert(capacity > 0);
-    size_t *ptr = ALLOC(sizeof(size_t) * 3 + sizeof(char) * (capacity + 1));
+    size_t *ptr = ALLOC(sizeof(size_t) * SLOT_COUNT + sizeof(char) * (capacity + 1));
     memset(ptr, 0, capacity + 1);
-    ptr[0] = capacity + 1; // capacity
-    ptr[1] = 0; // length
-    ptr[2] = 0xDEADC0DE; // magic
+    ptr[CAPACITY] = capacity + 1; // capacity
+    ptr[LENGTH] = 0; // length
+    ptr[MAGIC] = 0xDEADC0DE; // magic
     return to_str(ptr);
 }
 
 void freeString(char *s) {
     assert(stringIsValid(s));
     // Remove the magic number in case the string is used again accidentally.
-    from_str(s)[2] = 0;
+    from_str(s)[MAGIC] = 0;
     FREE(from_str(s));
 }
 
 bool stringIsValid(char *s) {
-    return from_str(s)[2] == 0xDEADC0DE;
+    return from_str(s)[MAGIC] == 0xDEADC0DE;
 }
 
 size_t stringLength(char *s) {
-    return from_str(s)[1];
+    return from_str(s)[LENGTH];
 }
 
 char *stringResize(char *s, size_t newCapacity) {
     assert(newCapacity > 0);
     size_t *ptr = from_str(s);
-    size_t oldCap = ptr[0];
+    size_t oldCap = ptr[CAPACITY];
     ptr = REALLOC(ptr, newCapacity);
     memset(ptr + oldCap, 0, labs(((ssize_t)oldCap) - ((ssize_t)newCapacity)));
-    ptr[0] = newCapacity;
+    ptr[CAPACITY] = newCapacity;
     return to_str(ptr);
 }
 
 char *stringNCopy(const char *s, int length) {
     char *str = newString(length);
     memcpy(str, s, length);
-    from_str(str)[1] = length;
+    from_str(str)[LENGTH] = length;
     return str;
 }
 
@@ -110,12 +117,11 @@ void stringAppend(char *dest, const char *format, ...) {
     vsnprintf(buffer, needed_length + 1, format, ap);
     va_end(ap);
 
-    // from_str(dest)[0] == capacity
-    if((size_t)(needed_length + 1) > from_str(dest)[0]) {
+    if((size_t)(needed_length + 1) > from_str(dest)[CAPACITY]) {
         stringResize(dest, needed_length + 1);
     }
     strncat(dest, buffer, needed_length);
-    // dest is filled with 0 by stringResize(), so no need to terminate the string.
+    // dest is zeroed by stringNew() & stringResize(), so no need to terminate the string.
     freeString(buffer);
     from_str(dest)[1] = needed_length;
 }
