1a74b84094170bf2f684d0a5f9a3d13a011ff4e3
Itai-Nelken <itainelken@gmail.com>
2022-10-16 19:12:50 +0300

    memory: fix ALLOC().
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/include/memory.h
+++ compiler/include/memory.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-#define ALLOC(size) malloc((size));
+#define ALLOC(size) malloc((size))
 #define CALLOC(nmemb, size) calloc((nmemb), (size))
 #define REALLOC(ptr, size) realloc((ptr), (size))
 #define FREE(p) free((p))
