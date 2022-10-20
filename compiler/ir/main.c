464e6704f70ac4feb57e709f84fa4595f80eca9d
Itai-Nelken <itainelken@gmail.com>
2022-10-20 18:05:45 +0300

    ir: fix program1
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/ir/main.c
+++ compiler/ir/main.c
@@ -16,23 +16,24 @@ OpCode program1[] = {
     ENCODE(OP_LEV),
 
     // fn test() { a += two(); }
-    ENCODE_ARG(OP_ENT, 0), // idx: 3
-    ENCODE_ARG(OP_LD, 0),
+    ENCODE_ARG(OP_ENT, 0), // idx: 4
     ENCODE_ARG(OP_CALL, 0),
+    ENCODE(OP_LR),
+    ENCODE_ARG(OP_LD, 0),
     ENCODE(OP_ADD),
     ENCODE_ARG(OP_ST, 0),
     ENCODE(OP_LEV),
 
-    // start (idx: 9)
+    // start (idx: 11)
     ENCODE_ARG(OP_IMM, 40),
     ENCODE_ARG(OP_ST, 0),
-    ENCODE_ARG(OP_CALL, 3),
+    ENCODE_ARG(OP_CALL, 4),
     ENCODE_ARG(OP_LD, 0)
 };
 Program prog1 = {
     .code = program1,
     .length = sizeof(program1)/sizeof(program1[0]),
-    .entry_point = 9
+    .entry_point = 11
 };
 
 
@@ -62,6 +63,7 @@ int main(int argc, char **argv) {
     if(argc == 2 && argv[1][0] == '-' && argv[1][1] == 'd' && argv[1][2] == 0) {
         debug_dump = true;
     }
+    //int result = execute(prog1.code, prog1.length, prog1.entry_point, debug_dump);
     int result = execute(prog2.code, prog2.length, prog2.entry_point, debug_dump);
     printf("result = %d\n", result);
     return 0;
