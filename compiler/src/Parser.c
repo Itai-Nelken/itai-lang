c57623521a9f0caf0468281b40135dc0aa5e6701
Itai-Nelken <itainelken@gmail.com>
2022-10-29 19:52:13 +0300

    Parser: use correct location in local redefinition error.
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/src/Parser.c
+++ compiler/src/Parser.c
@@ -408,7 +408,7 @@ static ASTNode *parse_function_body(Parser *p) {
         ASTObj *existing_obj = find_local(p, var_obj->as.var.name);
         if(existing_obj) {
             // TODO: emit hint of previous declaration using 'exisiting_obj.location'.
-            error(p, stringFormat("Redfinition of local variable '%s'.", var_obj->as.var.name));
+            error_at(p, var_node->location ,stringFormat("Redfinition of local variable '%s'.", var_obj->as.var.name));
             astNodeFree(var_node);
             // NOTE: arrayPop() is used even though we already have a reference
             //       to the object we want to free because we also want to remove
