4041125fd0a1c8fbf4a7624b6be8acf2dfcb768a
Itai-Nelken <itainelken@gmail.com>
2024-12-07 18:07:10 -0500

    AST: continue definition of requirements for each data structure
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

---
+++ compiler/include/Ast/StringTable.h
@@ -0,0 +1,10 @@
+#ifndef STRING_TABLE_H
+#define STRING_TABLE_H
+
+/**
+ * A StringTable holds a single copy of each string put in it.
+ * Attempting to add a string that already exists will return the already existing string.
+ * 
+ **/
+
+#endif // STRING_TABLE_H
--- compiler/include/Ast/Type.h
+++ compiler/include/Ast/Type.h
@@ -2,7 +2,12 @@
 #define AST_TYPE_H
 
 /**
- * A type is 
+ * Type represents a data type (such as i32, u32, char, str etc.) including pointer, function, and struct types.
+ * A Type stores:
+ *  - The source code location in which it was defined (which will be empty in the case of primitives that are not defined by the compiler).
+ *  - The module in which it was defined.
+ *  - The string representation of the type (e.g. "i32", "fn(i32, i32)->i32").
+ *  - Any information required for the type (e.g. parameter and return types for function types).
  **/
 
 #endif // AST_TYPE_H
