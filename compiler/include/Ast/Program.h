0004816aa64cab008adea2b6b058f0bab4d4dc21
Itai-Nelken <itainelken@gmail.com>
2025-06-04 22:42:45 +0300

    Ast: Program: decouple ModuleIDs from the actual idx of a module in the modules array.
    See explanation comment in Ast/Program.h for the reason why and how my solution works.
    
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- compiler/include/Ast/Program.h
+++ compiler/include/Ast/Program.h
@@ -10,15 +10,28 @@
  * A Program represents a complete AST tree for a program including modules, scopes, objects etc.
  * Effectively, it stores an Array of Modules, and owns all of them.
  * It also owns the StringTable for the entire program.
+ *
+ * Modules are a bit weird because they have a ModuleID that allows for a single module to be
+ * represented by multiple instances of ASTModule over the compilation process (only one at a time though).
+ * However, the modules array itself won't necessarily have the same order, for example the validator
+ * sorts the module by their dependency on each other (imports).
+ * The solution I'm using is having a table that maps moduleIDs to the actual index of each module
+ * in the modules array. That way the order in which the modules are stored doesn't matter
+ * and doesn't change the ModuleIDs.
+ * The reason I'm using a hashtable even though an array would suffice theoretically
+ * is that I need to be able to insert ModuleIDs out of order, and my Table interface/impl
+ * provides that ability.
  **/
 typedef struct ast_program {
     StringTable *strings;
     Array modules; // Array<ASTModule *>;
+    Table moduleIDToIdx; // Table<ModuleID, usize>
 } ASTProgram;
 
-// A ModuleID is an index into the modules array.
+// A ModuleID is an index into the moduleIDToIdx array.
 // It is used since it can represent an equivalent module in two different programs
 // (such as a parsed program and a checked program).
+// see explanation above ASTProgram definition for what it actually indexes.
 typedef usize ModuleID;
 
 
@@ -53,6 +66,16 @@ void astProgramFree(ASTProgram *prog);
  * @return The ModuleID for the newly created module.
  **/
 ModuleID astProgramNewModule(ASTProgram *prog, ASTString name);
+
+/**
+ * Creates a new ASTModule associated with a specific ModuleID and adds it to the internal list.
+ *
+ * @param prog The ASTProgram to add a module in.
+ * @param id The ID to associate the module with.
+ * @param name The name of the module to create.
+ * @return The ModuleID for the newly created module.
+ **/
+void astProgramNewModuleWithID(ASTProgram *prog, ModuleID id, ASTString name);
 
 /**
  * Get an ASTModule using its ModuleID.
--- compiler/src/Ast/Program.c
+++ compiler/src/Ast/Program.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "common.h"
 #include "Array.h"
+#include "Table.h"
 #include "Ast/StringTable.h"
 #include "Ast/Module.h"
 #include "Ast/Program.h"
@@ -10,6 +11,16 @@
 static void free_module_callback(void *module, void *cl) {
     UNUSED(cl);
     astModuleFree((ASTModule *)module);
+}
+
+static unsigned int hashModuleId(void *mID) {
+    return (usize)mID;
+}
+
+static bool cmpModuleId(void *a, void *b) {
+    ModuleID aID = (ModuleID)a;
+    ModuleID bID = (ModuleID)b;
+    return aID == bID;
 }
 
 
@@ -36,23 +47,39 @@ void astProgramPrint(FILE *to, ASTProgram *prog) {
 
 void astProgramInit(ASTProgram *prog, StringTable *st) {
     arrayInit(&prog->modules);
+    tableInit(&prog->moduleIDToIdx, hashModuleId, cmpModuleId);
     prog->strings = st;
 }
 
 void astProgramFree(ASTProgram *prog) {
     arrayMap(&prog->modules, free_module_callback, NULL);
     arrayFree(&prog->modules);
+    // nothing to free here
+    tableFree(&prog->moduleIDToIdx);
 }
 
 ModuleID astProgramNewModule(ASTProgram *prog, ASTString name) {
     ASTModule *m = astModuleNew(name);
-    // Note: cast is uneccesary, but is included to make code more readable and understandable.
-    return (ModuleID)arrayPush(&prog->modules, (void *)m);
+    usize actualIdx = arrayPush(&prog->modules, (void *)m);
+    tableSet(&prog->moduleIDToIdx, (void *)actualIdx, (void *)actualIdx);
+    m->id = actualIdx;
+    // Note: the cast is uneccesary because ModuleID is actually usize,
+    // but it is included to make the code more readable and understandable.
+    return (ModuleID)actualIdx;
+}
+
+void astProgramNewModuleWithID(ASTProgram *prog, ModuleID id, ASTString name) {
+    ASTModule *m = astModuleNew(name);
+    m->id = id;
+    usize actualIdx = arrayPush(&prog->modules, (void *)m);
+    tableSet(&prog->moduleIDToIdx, (void *)id, (void *)actualIdx);
 }
 
 ASTModule *astProgramGetModule(ASTProgram *prog, ModuleID id) {
-    VERIFY(id < arrayLength(&prog->modules));
-    ASTModule *m = ARRAY_GET_AS(ASTModule *, &prog->modules, id);
+    TableItem *item = tableGet(&prog->moduleIDToIdx, (void *)id);
+    VERIFY(item);
+    usize actualIdx = (usize)item->value;
+    ASTModule *m = ARRAY_GET_AS(ASTModule *, &prog->modules, actualIdx);
     VERIFY(m);
     return m;
 }
